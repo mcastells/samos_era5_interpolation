49d2fd5ce6a48476e52e79b5bea82639359c0c7a
Marc Castells <mcastells@coaps.fsu.edu>
2024-03-04 17:43:37 -0500

    initial commit

---
+++ era5.py
@@ -0,0 +1,125 @@
+#https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=overview
+
+from datetime import timedelta
+
+import cdsapi
+import numpy as np
+import pygrib
+from metpy.units import masked_array, units
+from numpy import ma
+
+c = cdsapi.Client()
+
+class Era5:
+    def __init__(self, gribs, dt_start, dt_end) -> None:
+        self.timesteps = []
+        self.lons = None
+        self.lats = None
+        self.lons_ocean = None
+        self.lats_ocean = None
+
+        #self.wind_10m = []
+        self.u_wind_10m = []
+        self.v_wind_10m = []
+        self.mslp = []
+        self.temp_2m = []
+        self.dewpoint_2m = []
+        self.sst = []
+
+        for grb in gribs:
+            if grb.validDate >= dt_start and grb.validDate <= dt_end:
+                if grb.validDate not in self.timesteps:
+                    self.timesteps.append(grb.validDate)
+                if '10 metre U wind component' in grb.parameterName: 
+                    self.u_wind_10m.append(grb.values)
+                elif '10 metre V wind component' in grb.parameterName: 
+                    self.v_wind_10m.append(grb.values)
+                elif 'Mean sea level pressure' in grb.parameterName: 
+                    self.mslp.append(grb.values)
+                elif '2 metre temperature' in grb.parameterName: 
+                    self.temp_2m.append(grb.values)
+                elif '2 metre dewpoint' in grb.parameterName: 
+                    self.dewpoint_2m.append(grb.values)
+                elif grb.parameterName == 'Sea surface temperature': 
+                    self.sst.append(grb.values)
+                    if self.lats_ocean is None and self.lons_ocean is None:
+                        self.lats_ocean, self.lons_ocean = grb.latlons()
+                else:
+                    raise(Exception(f'Match not found for parameter name: {grb.parameterName}'))
+        self.timesteps = np.array(self.timesteps)
+        ntimesteps = len(self.timesteps)
+        
+        self.lats, self.lons = grb.latlons()  # get the lats and lons for the grid.
+        nlats_atmos = self.lats.shape[0]
+        nlons_atmos = self.lats.shape[1]
+        if self.lats_ocean is not None:
+            nlats_ocean = self.lats_ocean[0]
+            nlons_ocean = self.lats_ocean[1]
+        else:
+            nlats_ocean = 0
+            nlons_ocean = 0
+
+        def invalidate_data_missing_timesteps(data,grid):
+            return data if len(data) == ntimesteps else ma.masked_all(shape=(ntimesteps,nlats_atmos,nlons_atmos) if grid=='atmos' else (ntimesteps,nlats_ocean,nlons_ocean))
+
+        def add_units(data, unit_name, grid='atmos'):
+            return masked_array(invalidate_data_missing_timesteps(data,grid),units(unit_name))
+        
+        self.u_wind_10m = add_units(self.u_wind_10m, 'm/s')
+        self.v_wind_10m = add_units(self.v_wind_10m, 'm/s')
+        self.mslp = add_units(self.mslp, 'Pa')
+        self.temp_2m = add_units(self.temp_2m, 'K')
+        self.sst = add_units(self.sst, 'K')
+
+        #self.wind_10m = (self.u_wind_10m**2 + self.v_wind_10m**2)**0.5
+
+
+def download(dt_start, dt_end, data_extent, filename):
+    years = []
+    months = []
+    days = []
+    date_iter = dt_start.replace(hour=0)
+    while date_iter <= dt_end + timedelta(days=1):
+        if str(date_iter.year) not in years:
+            years.append(str(date_iter.year))
+        if str(date_iter.month) not in months:
+            months.append(str(date_iter.month))
+        if str(date_iter.day) not in days:
+            days.append(str(date_iter.day))
+        date_iter += timedelta(days=1)
+
+    era5_variables = ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_temperature', '2m_dewpoint', 'mean_sea_level_pressure', 'sea_surface_temperature']
+
+    c.retrieve(
+        'reanalysis-era5-single-levels',
+        {
+            'product_type': 'reanalysis',
+            'format': 'grib',
+            'variable': era5_variables,
+            'year': years,
+            'month': months,
+            'day': days,
+            'time': [
+                '00:00', '01:00', '02:00',
+                '03:00', '04:00', '05:00',
+                '06:00', '07:00', '08:00',
+                '09:00', '10:00', '11:00',
+                '12:00', '13:00', '14:00',
+                '15:00', '16:00', '17:00',
+                '18:00', '19:00', '20:00',
+                '21:00', '22:00', '23:00',
+            ],
+            'area': [
+                data_extent[3], data_extent[0], data_extent[2],
+                data_extent[1],
+            ],
+        },
+        filename)
+    gribs = pygrib.open(filename)
+
+    for grb in gribs[1:len(era5_variables)]:
+        print(grb.parameterName)
+        print(f'\t{grb.units}')
+    gribs.rewind()
+
+    return gribs
---
+++ main.py
@@ -0,0 +1,100 @@
+import datetime
+from pathlib import Path
+
+import numpy as np
+import numpy.ma as ma
+import pandas
+import pygrib
+import xarray as xr
+from metpy.interpolate import interpolate_to_points
+from termcolor import colored
+from tqdm import tqdm, trange
+
+import era5
+
+samos_input_dir = Path('/Net/mdc/marineflux/data/SAMOS_Fluxes_2024-02-13/B23')
+era5_dir = Path('~/era5_data/')
+
+def interpolate_timestep(previoushour, nexthour, minute):
+    weight = minute/60.
+    return previoushour * (1-weight) + nexthour * weight
+
+if __name__ == '__main__':
+    ncpaths = sorted(list(samos_input_dir.rglob('*.nc')))
+
+    for path in tqdm(ncpaths, desc=f'Processing {samos_input_dir.name} files', leave=False):
+        d = path.parent
+        versions = sorted(d.glob(path.stem[0:-4]+'*.nc'))
+        
+        # path == versions[-1] means that this file is the latest version
+        if path.is_file() and path.suffix == '.nc' and path == versions[-1]:
+
+            tqdm.write(f'Opening {colored(path, "blue")}')
+            samos_ds = xr.open_dataset(path)
+
+            timepadding = 1
+            # Remove minutes from datetime for synoptic plot
+            synoptic_dt_start = pandas.to_datetime(samos_ds.time.data[0]) - datetime.timedelta(hours=timepadding)
+            synoptic_dt_end = pandas.to_datetime(samos_ds.time.data[-1]) + datetime.timedelta(hours=timepadding)
+
+            extent_padding = 1
+            extent = [ma.min(samos_ds.lon[:]) - extent_padding, ma.max(samos_ds.lon[:]) + extent_padding, ma.min(samos_ds.lat[:]) - extent_padding, ma.max(samos_ds.lat[:]) + extent_padding]
+            
+            grib_filepath = f'{era5_dir / path.stem}_{datetime.datetime.strftime(synoptic_dt_start,"%Y%m%d-%H%M")}_{datetime.datetime.strftime(synoptic_dt_end,"%Y%m%d-%H%M")}_era5.grib'
+            grib_p = Path(grib_filepath)
+            if not grib_p.is_file():
+                # if a grib file with the specific datetime range doesn't exist, look for grib files with a wider datetime range
+                for p in list(era5_dir.glob(f'{path.stem}*.grib')):
+                    p_split = p.name.split('_')
+                    grib_start = datetime.datetime.strptime(p_split[-3],"%Y%m%d-%H%M")
+                    grib_end = datetime.datetime.strptime(p_split[-2],"%Y%m%d-%H%M")
+                    if grib_start <= synoptic_dt_start and grib_end >= synoptic_dt_end:
+                        grib_p = p
+                        break
+
+            try:
+                grbs = pygrib.open(str(grib_p))
+            except Exception as error:
+                grbs = era5.download(synoptic_dt_start, synoptic_dt_end, extent, grib_filepath)
+
+            era5_ds = era5.Era5(grbs,synoptic_dt_start,synoptic_dt_end)
+
+            era5_interpolated_to_ship = {
+                'uwind_10m': np.empty(shape=len(samos_ds.time)),
+                'vwind_10m': np.empty(shape=len(samos_ds.time)),
+                'mslp': np.empty(shape=len(samos_ds.time)),
+                'temp_2m': np.empty(shape=len(samos_ds.time)),
+                'dewpoint_2m': np.empty(shape=len(samos_ds.time)),
+                'sst': np.empty(shape=len(samos_ds.time))
+            }
+
+            syn_timesteps = era5_ds.timesteps
+
+            for era5_var_name in era5_interpolated_to_ship.keys():
+                if era5_var_name == 'sst':
+                    syn_lons = era5_ds.lons_ocean
+                    syn_lats = era5_ds.lats_ocean
+                else:
+                    syn_lons = era5_ds.lons
+                    syn_lats = era5_ds.lats
+                
+                era5_var = getattr(era5_ds, era5_var_name)
+
+                syn_lons[np.where(syn_lons < 0)] += 360
+
+                points = np.array((syn_lons.flatten(),syn_lats.flatten())).T
+
+                for i in trange(len(samos_ds.time), desc='Interpolating minute ERA5 variables', leave=False):
+                    dt = samos_ds.time[i]
+                    if dt.minute == 0:
+                        timestep = np.where(syn_timesteps == dt)[0][0]
+                        era5var_atship = interpolate_to_points(points=points,values=era5_var[timestep].m.flatten(),xi=(samos_ds.lon[i],samos_ds.lat[i]))
+                    else:
+                        previoushour = np.where(syn_timesteps < dt)[0][-1]
+                        era5var_attime = interpolate_timestep(era5_var[previoushour].m,era5_var[previoushour+1].m,dt.minute)
+                        era5var_atship = interpolate_to_points(points=points,values=era5var_attime.flatten(),xi=(samos_ds.lon[i],samos_ds.lat[i]))
+                    era5_interpolated_to_ship[i] = float(era5var_atship)
+
+            print(era5_interpolated_to_ship)
+
+            # calculate fluxes using these interpolated values
